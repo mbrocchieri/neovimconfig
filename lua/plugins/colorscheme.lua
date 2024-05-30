ad398a7e29c58175dfc014d3a8abb9c388f25604
Mathieu Brocchieri <mathieu-externe.brocchieri@datanumia.com>
2024-05-30 16:08:17 +0200

    clean unused theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
-	},
-
-	{
 		-- Theme inspired by Atom
 		"navarasu/onedark.nvim",
 		priority = 1000,
