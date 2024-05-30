23f3461e3a58052a6c51071f8455a38497ab047a
Mathieu Brocchieri <mathieu-externe.brocchieri@datanumia.com>
2024-05-30 16:09:23 +0200

    remove c cpp go from ensure installed in treesitter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -86,7 +86,7 @@ vim.keymap.set("n", "<leader>tt", require("nvim-tree.api").tree.toggle, {
 -- See `:help nvim-treesitter`
 require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "tsx", "typescript", "vimdoc", "vim", "java" },
+	ensure_installed = { "lua", "python", "typescript", "vimdoc", "vim", "java" },
 
 	-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
 	auto_install = true,
